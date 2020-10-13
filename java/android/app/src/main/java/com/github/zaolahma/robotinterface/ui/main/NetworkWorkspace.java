5958fa83a80a0eb53b5fd70e9dc534b5ba20d18c
ZaoLahma <thyberg.jan@gmail.com>
2020-10-13 21:42:06 +0200

    Button prints text

--- java/android/app/src/main/java/com/github/zaolahma/robotinterface/ui/main/NetworkWorkspace.java
+++ java/android/app/src/main/java/com/github/zaolahma/robotinterface/ui/main/NetworkWorkspace.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,7 +12,7 @@ import androidx.annotation.Nullable;
 
 import com.github.zaolahma.robotinterface.R;
 
-public class NetworkWorkspace extends WorkspaceBase {
+public class NetworkWorkspace extends WorkspaceBase implements View.OnClickListener {
     @Override
     public String getWorkspaceName() {
         return "Network";
@@ -21,6 +22,13 @@ public class NetworkWorkspace extends WorkspaceBase {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.network_workspace, container, false);
+        Button button = (Button) root.findViewById(R.id.nw_connect_button);
+        button.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View v) {
+        System.out.println("Connect!");
     }
 }
