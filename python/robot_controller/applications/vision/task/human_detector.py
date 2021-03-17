5b82a5d5d0917dd98449b53464852b72d4010992
ZaoLahma <thyberg.jan@gmail.com>
2021-03-17 17:41:22 +0100

    Add detection of human bodies and faces

--- python/robot_controller/applications/vision/task/human_detector.py
+++ python/robot_controller/applications/vision/task/human_detector.py
@@ -12,4 +12,15 @@ class HumanDetector():
     def detect_humans(self, image):
         humans = []
 
+        bodies = self.detect_bodies(image)
+        faces = self.detect_faces(image)
+
         return humans
+
+    def detect_faces(self, image):
+        faces = self.face_cascade.detectMultiScale(image, 1.1, 4)
+        return faces
+
+    def detect_bodies(self, image):
+        (rects, weights) = self.hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        return rects
