6de40114a1775360e924afb438bd6f481b370d27
ZaoLahma <thyberg.jan@gmail.com>
2021-04-07 17:42:10 +0200

    issue_24 Adjust values... again

--- python/robot_controller/applications/mind/hide/hide_task.py
+++ python/robot_controller/applications/mind/hide/hide_task.py
@@ -59,10 +59,10 @@ class HideTask(TaskBase):
         msg = self.comm_if.get_message(SonarDataInd.get_msg_id())
         if None != msg:
             Log.log("MOVE DISTANCE: " + str(msg.distance))
-            if msg.distance < 500:
+            if msg.distance < 600:
                 move_ind = MoveInd(MoveInd.RIGHT, 100, 2)
                 self.comm_if.send_message(move_ind)
-                self.turn_time = 2
+                self.turn_time = 4
                 self.turn_exit_state = "RELEASE_RESOURCES"
                 self.state_handler.transition()
             else:
